6a73f30d5dbf81188aeea302c1820d07014627a2
saremipegah <saremipegah@gmail.com>
2022-07-27 09:35:15 +0430

    Restaurant menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import menus from './data'
+import './App.css'
+import Categories from './Components/Categories'
+import Menu from './Components/Menu'
+
+
+
+const allCategorise = [ 'all',...new Set( menus.map(menu => menu.category))]
 
 function App() {
+
+  
+
+  const [allMenus,setAllMenus] = useState(menus)
+  const [categories , setCategorise]= useState(allCategorise)
+ 
+  const filterMenus = (category) => {
+    if(category ==='all'){
+      setAllMenus(menus)
+      return 
+    }
+    let filteredMenus = menus.filter(menu => menu.category === category)
+    setAllMenus(filteredMenus)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='menu section'>
+        <div className="title">
+          <h2>Restaurant menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterMenus={filterMenus}/>
+        <Menu allMenus={allMenus}/>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
